f7a3809c84079e219a8db138959ea5ebf1ba5518
Adam Mohammed <adammoham@gmail.com>
2020-09-28 19:45:56 -0400

    Creates s3 keys with 30 second lifetimes

--- backend.go
+++ backend.go
@@ -1,29 +1,54 @@
 package s3key
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/vault/sdk/framework"
-	"github.com/hashicorp/vault/sdk/helper/jsonutil"
 	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/linode/linodego"
+	"golang.org/x/oauth2"
+	"net/http"
+	"os"
+	"time"
 )
 
 type backend struct {
 	*framework.Backend
-	store map[string][]byte
+	client linodego.Client
+	store  map[string][]byte
+}
+
+type S3Credential struct {
+	ID        int
+	AccessKey string
+	SecretKey string
+}
+
+func CreateLinodeClient(token string) linodego.Client {
+	transportClient := &http.Client{
+		Transport: &oauth2.Transport{
+			Source: oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token}),
+		},
+	}
+
+	return linodego.NewClient(transportClient)
 }
 
 func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend, error) {
-	b := &backend {
 
+	b := &backend{
+		store: make(map[string][]byte),
 	}
 
 	b.Backend = &framework.Backend{
-		Help: "Test backend",
-		BackendType: logical.TypeLogical,
+		Help:           "Test backend",
+		BackendType:    logical.TypeLogical,
+		InitializeFunc: b.initializer(),
 	}
 
-	b.Backend.Paths = append(b.Backend.Paths, paths()...)
+	b.Backend.Paths = append(b.Backend.Paths, b.paths()...)
 
 	if conf == nil {
 		return nil, fmt.Errorf("configuration passed into backend is nil")
@@ -31,30 +56,52 @@ func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend,
 
 	b.Backend.Setup(ctx, conf)
 
-
 	return b, nil
 }
 
+func (b *backend) initializer() framework.InitializeFunc {
+	return func(context.Context, *logical.InitializationRequest) error {
+		f, err := os.Open("linode-token")
+
+		if err != nil {
+			return fmt.Errorf("Couldn't open token file: %v\n", err)
+		}
+
+		scanner := bufio.NewScanner(f)
+		var token string
+		if scanner.Scan() {
+			token = scanner.Text()
+		}
+
+		if token == "" {
+			token = "default-token"
+			b.Backend.Logger().Warn("using default-token")
+		}
+		b.Backend.Logger().Info("Creating Linode Client")
+		b.client = CreateLinodeClient(token)
+		return nil
+	}
+}
+
 func (b *backend) paths() []*framework.Path {
-	return []*framework.Path {
+	return []*framework.Path{
 		{
 			Pattern: framework.MatchAllRegex("path"),
 			Fields: map[string]*framework.FieldSchema{
-				Type: logical.TypeString,
-				Description: "Specifies the path of the secret.",
+				"path": {
+					Type:        framework.TypeString,
+					Description: "Specifies the path of the secret.",
+				},
 			},
 			Operations: map[logical.Operation]framework.OperationHandler{
 				logical.ReadOperation: &framework.PathOperation{
 					Callback: b.handleRead,
+
 					Summary: "Read the secret.",
-				},
-				logical.CreateOperation: &framework.PathOperation{
-					Callback: b.handleCreate,
-					Summary: "Create secrets.",
 				},
 				logical.DeleteOperation: &framework.PathOperation{
 					Callback: b.handleDelete,
-					Summary: "Deletes the secret.",
+					Summary:  "Deletes the secret.",
 				},
 			},
 
@@ -62,7 +109,6 @@ func (b *backend) paths() []*framework.Path {
 		},
 	}
 }
-
 
 func (b *backend) handleExistenceCheck(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
 	out, err := req.Storage.Get(ctx, req.Path)
@@ -80,51 +126,55 @@ func (b *backend) handleRead(ctx context.Context, req *logical.Request, data *fr
 	}
 
 	path := data.Get("path").(string)
-
-	// Decode the data
-	var rawData map[string]interface{}
-	if err := jsonutil.DecodeJSON(b.store[req.ClientToken+"/"+path], &rawData); err != nil {
-		return nil, errwrap.Wrapf("json decoding failed: {{err}}", err)
+	opts := linodego.ObjectStorageKeyCreateOptions{
+		Label: path,
 	}
+
+	key, err := b.client.CreateObjectStorageKey(context.TODO(), opts)
+
+	if err != nil {
+		return nil, errwrap.Wrapf("key create failed: {{err}}", err)
+	}
+
+	keyData := S3Credential{
+		ID:        key.ID,
+		AccessKey: key.AccessKey,
+		SecretKey: key.SecretKey,
+	}
+
+	keys := make(map[string]interface{}, 3)
+	keys["ID"] = keyData.ID
+	keys["AccessKey"] = keyData.AccessKey
+	keys["SecretKey"] = keyData.SecretKey
+
+	go func() {
+		lifetime := time.After(30 * time.Second)
+		select {
+
+		case <-lifetime:
+			err := b.client.DeleteObjectStorageKey(context.TODO(), keyData.ID)
+			if err != nil {
+				b.Logger().Warn("Couldn't delete key [%v]", keyData.ID)
+			}
+		}
+	}()
 
 	// Generate the response
 	resp := &logical.Response{
-		Data: rawData,
+		Data: keys,
 	}
 
 	return resp, nil
 }
 
-
-func (b *backend) handleCreate(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-	if req.ClientToken == "" {
-		return nil, fmt.Errorf("client token empty")
-	}
-
-	if len(req.Data) == 0 {
-		return nil, fmt.Errorf("data must be provide to store in secret")
-	}
-
-	path := data.Get("path").(string)
-
-	buf, err := json.Marshal(req.Data)
-	if err != nil {
-		return nil, errwrap.Wrapf("json encoding failed: {{err}}", err)
-	}
-
-	b.store[req.ClientToken+"/"+path] = buf
-
-	return nil, nil
-}
-
-func (b *backend) handleDelete(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logicalResponse, error) {
+func (b *backend) handleDelete(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	if req.ClientToken == "" {
 		return nil, fmt.Errorf("client token empty")
 	}
 
 	path := data.Get("path").(string)
 
-	delete(b.store, path)
+	delete(b.store, req.ClientToken+"/"+path)
 
 	return nil, nil
 }
--- cmd/s3key/main.go
+++ cmd/s3key/main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-    "os"
+	"os"
 
-    s3key "github.com/adammohammed/s3-key-plugin"
-    "github.com/hashicorp/vault/api"
-    "github.com/hashicorp/vault/sdk/plugin"
+	s3key "github.com/adammohammed/s3-key-plugin"
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/plugin"
 )
 
 func main() {
-    apiClientMeta := &api.PluginAPIClientMeta{}
-    flags := apiClientMeta.FlagSet()
-    flags.Parse(os.Args[1:])
+	apiClientMeta := &api.PluginAPIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
 
-    tlsConfig := apiClientMeta.GetTLSConfig()
-    tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
 
-    err := plugin.Serve(&plugin.ServeOpts{
-        BackendFactoryFunc: s3key.Factory,
-        TLSProviderFunc:    tlsProviderFunc,
-    })
-    if err != nil {
-        logger := hclog.New(&hclog.LoggerOptions{})
+	err := plugin.Serve(&plugin.ServeOpts{
+		BackendFactoryFunc: s3key.Factory,
+		TLSProviderFunc:    tlsProviderFunc,
+	})
+	if err != nil {
+		logger := hclog.New(&hclog.LoggerOptions{})
 
-        logger.Error("plugin shutting down", "error", err)
-        os.Exit(1)
-    }
+		logger.Error("plugin shutting down", "error", err)
+		os.Exit(1)
+	}
 }
